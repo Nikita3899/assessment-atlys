a57893531cb5eaf98c5b15123b0d329904355594
Nikita Sharma <nikitasharma3899@gmail.com>
2023-11-05 22:49:34 +0530

    user credentials authenticated

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,42 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import style from './formCompoent.module.scss'
 import clsx from 'clsx';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, message } from 'antd';
 
 const FormComponent = () => {
 
-  const [userDetails, setUserDetails] = useState([{
+  const [userDetails, setUserDetails] = useState({
     userName:'',
     email:'',
     password:''
-  }]);
+  });
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isShowDashboard, setIsShowDashboard] = useState(false)
+
+  useEffect(() => {
+    // Check if the user is already registered in local storage
+    console.log(userDetails,'jhgfhbj')
+    const storedUserDetails = localStorage.getItem('userDetails');
+    if (storedUserDetails) {
+      setUserDetails(JSON.parse(storedUserDetails));
+    }
+  }, [userDetails.password]);
 
   const handleLogIn = () =>{
     setIsLoggedIn(!isLoggedIn); 
   }
 
   const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
-    setIsLoggedIn(true);
-  };
+    const storedUserDetails = localStorage.getItem('userDetails');
 
- 
+    if(storedUserDetails){
+
+        const userStored = JSON.parse(storedUserDetails);
+        if((values.usernameEmail === userStored.userName || values.email === userStored.email) && values.password === userStored.password ){
+            message.success('LogIn Successfull')
+            setIsShowDashboard(true)
+        }else{
+            message.error('Incorrect Credentials!')
+        }
+        
+    }
+  };
 
   const onRegisteration = (values:any) =>{
     const {username,email, password} = values;
-   
-    setUserDetails((ele:any)=>({
-      ...ele,
+
+      localStorage.setItem('userDetails', JSON.stringify({ userName: username, email, password }));
+
+    setUserDetails({
       userName:username,
       email:email,
       password:password
-    }))
+  })
 
+  message.success('Registeration Successfull');
+  setIsLoggedIn(true)
   }
-
-
 
   return (
     <div className={style['main-container']}>
@@ -59,7 +79,8 @@ const FormComponent = () => {
         className={style['label-text']}
         rules={[{ required: true, message:'This field is required!'}]}
       >
-        <Input placeholder={isLoggedIn ? 'Enter your email or username' : 'Enter your email'} className={style['input-text']}/>
+        <Input placeholder={isLoggedIn ? 'Enter your email or username' : 'Enter your email'} className={style['input-text']}
+         />
       </Form.Item>
       {!isLoggedIn && 
       <Form.Item
@@ -91,6 +112,7 @@ const FormComponent = () => {
           type="password"
           placeholder={isLoggedIn ? "Enter your password" : "Choose a strong password" }
           className={style['input-text']}
+          
         />
         </div>    
       </Form.Item>
